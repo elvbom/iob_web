aa7e1bc5a60a9c9a541d1ef880d3229d1957cf5c
Elvira Boman <you@example.com>
2016-06-02 16:27:32 +0000

    nya gifsidan

---
+++ iob.js
@@ -0,0 +1,11 @@
+$('a.linkedin').click(function(e) {
+    e.preventDefault();
+    window.open('https://www.linkedin.com/in/lovisa-lugneg%C3%A5rd-b101ba91');
+    window.open('https://www.linkedin.com/in/elviraboman?trk=pub-pbmap');
+});
+
+$('a.github').click(function(e) {
+    e.preventDefault();
+    window.open('https://github.com/LovisaLugnegard');
+    window.open('https://github.com/elvbom');
+});
